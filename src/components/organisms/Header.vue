80c24507e838c48a43ffd16b8d7172d7d9593b36
jjlabrada <jjlabrada@xetid.cu>
2025-12-23 21:41:03 -0500

    header

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Trophy from '@/assets/images/icon-personal-best.svg?url';
+import { useGameStore } from '@/store/game.store';
+import { useTestStore } from '@/store/test.store';
+import Logo from '../atoms/Logo.vue';
+const store = useGameStore()
+const store2 = useTestStore()
+</script>
+
+<template>
+  <header class="flex items-center justify-between">
+    <Logo />
+    {{ store2.elapsedTime }}
+    <div class="text-tp4 gap-2.5 flex items-center">
+      <img :src="Trophy" alt="">
+      <p><span class="text-neutral-400 ">
+          Personal best:</span> {{ store.personalRecord }} WPM</p>
+
+    </div>
+  </header>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Header from '@/components/organisms/Header.vue';
 
 
 </script>
@@ -6,6 +7,7 @@
 <template>
   <main
     class="relative px-4 md:px-8 lg:px-28 py-4 md:py-8 flex flex-col gap-8 md:gap-20 lg:gap-16 bg-neutral-900 text-neutral-0 min-h-screen text-base font-sora">
+    <Header />
 
   </main>
 </template>
